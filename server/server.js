564da0c8cb88af938c09c6c7f19a34b26d1c62a1
Kellie Ryan <littlekel03@gmail.com>
2021-08-12 20:34:52 +0800

    Updated scripts and server files for develop mode

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
@@ -14,6 +14,14 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
