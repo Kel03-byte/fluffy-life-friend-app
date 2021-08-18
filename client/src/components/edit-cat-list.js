b88ddaf443144db52e79676616abd448933980fa
Kellie Ryan <littlekel03@gmail.com>
2021-08-18 10:21:13 +0800

    Updated edit page

--- client/src/components/edit-cat-list.js
+++ client/src/components/edit-cat-list.js
@@ -1,36 +1,18 @@
 // List of Adoptable Cats Component
 
 import React from 'react';
-// import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
-// import { REMOVE_CAT } from '../utils/mutations';
 
 const EditList = ({ cats }) => {
-    // let { id } = useParams()
-    // const catId = id
-    // const [removeCat, { error }] = useMutation(REMOVE_CAT);
-
-    // const removeCatProfile = async () => {
-    //     try {
-    //         await removeCat(catId);
-    //     } catch (error) {
-    //         console.error(error.message);
-    //     }
-    // };
-
     return (
         <div id='edit-list'>
             {cats &&
                 cats.map((cat) => (
                     <div key={cat._id}>
-                        <p>{cat.name}!</p>
-                        <button id='edit-button'><Link id='nav-text' to={`/edit/${cat._id}`}>Edit</Link></button>
-                        {/* <button onClick={removeCatProfile}>Delete</button> */}
+                        <button id='list-button' ><Link className='list-text' to={`/edit/${cat._id}`}>{cat.name}</Link></button>
                         <br />
                     </div>
                 ))}
-                {/* {error && <div>{error.message}</div>} */}
         </div>
     );
 };
--- client/src/pages/cat-worker/edit/single-cat/edit-cat.js
+++ client/src/pages/cat-worker/edit/single-cat/edit-cat.js
@@ -1,211 +1,44 @@
 // Edit A Single Cat Page
 
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { useParams } from 'react-router';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_SINGLE_CAT } from '../../../../utils/queries';
-import { UPDATE_CAT, REMOVE_CAT } from '../../../../utils/mutations';
+import { REMOVE_CAT } from '../../../../utils/mutations';
 import './edit-cat.css'
 
 const EditCat = () => {
     let { id } = useParams()
     const catId = id
-    const { loading, error, data } = useQuery(QUERY_SINGLE_CAT, {
+    const { data, loading, error } = useQuery(QUERY_SINGLE_CAT, {
         variables: { catId: catId }
     })
 
-    const [updateCat, { error: updateError }] = useMutation(UPDATE_CAT);
     const [removeCat, { data: removeData, error: removeError }] = useMutation(REMOVE_CAT);
 
-    const [catName, setCatName] = useState();
-    const [dob, setDob] = useState();
-    const [sex, setSex] = useState();
-    const [isShy, setIsShy] = useState();
-    const [dogs, setDogs] = useState();
-    const [cats, setCats] = useState();
-    const [childU8, setChildU8] = useState();
-    const [child8to13, setChild8t13] = useState();
-    const [specialReq, setSpecReq] = useState();
 
     const removeCatProfile = async (event) => {
         event.preventDefault()
         removeCat({
-            variables: {catId: catId}})
+            variables: { catId: catId }
+        })
     };
-    
-    const handleInputChange = (event) => {
-        let target = event.target;
-        let inputType = target.name;
-        let inputValue = target.value;
-        
-        if (inputType === 'catName') {
-            setCatName(inputValue);
-        } else if (inputType === 'dob') {
-            setDob(inputValue);
-        } else if (inputType === 'sex') {
-            setSex(inputValue);
-        } else if (inputType === 'isShy') {
-            setIsShy(inputValue);
-        } else if (inputType === 'dogs') {
-            setDogs(inputValue);
-        } else if (inputType === 'cats') {
-            setCats(inputValue);
-        } else if (inputType === 'childU8') {
-            setChildU8(inputValue);
-        } else if (inputType === 'child8t13') {
-            setChild8t13(inputValue);
-        } else if (inputType === 'specialReq') {
-            setSpecReq(inputValue);
-        }
-    };
-    
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            await updateCat({
-                variables: {
-                    catId: catId,
-                    name: catName,
-                    dob: dob,
-                    sex: sex,
-                    shy: isShy,
-                    dogs: dogs,
-                    otherCats: cats,
-                    childU8: childU8,
-                    child8to13: child8to13,
-                    specialReq: specialReq,
-                },
-            })
-        } catch (error) {
-            console.log(error.message);
-        }
-        setCatName('');
-        setDob('');
-        setCats('');
-        setDogs('');
-        setIsShy('');
-        setChild8t13('');
-        setChildU8('');
-        setSex('');
-        setSpecReq('');
-    };
-    
+
+
     return (
         <main>{loading ? (
             <div>Loading...</div>
         ) : (
             <Fragment>
                 <div id='edit-cat-page'><br />
-                    <p id='edit-cat-title'>Update {data.cat.name}'s details!</p>
-                    <form id='add-form'>
-                        <div id='add-box'>
-                            <label>Update {data.cat.name}'s name:</label><br />
-                            <input
-                                id='add-input'
-                                value={catName}
-                                name="catName"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="cat's name"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update {data.cat.name}'s DOB:</label><br />
-                            <input
-                                id='add-input'
-                                value={dob}
-                                name="dob"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="DD/MM/YYYY"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update {data.cat.name}'s' sex:</label><br />
-                            <input
-                                id='add-input'
-                                value={sex}
-                                name="sex"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Male/Female"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update if {data.cat.name} is shy:</label><br />
-                            <input
-                                id='add-input'
-                                value={isShy}
-                                name="isShy"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Yes/No"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update if {data.cat.name} is use to other cats:</label><br />
-                            <input
-                                id='add-input'
-                                value={cats}
-                                name="cats"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Yes/No"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update if {data.cat.name} is use to dogs:</label><br />
-                            <input
-                                id='add-input'
-                                value={dogs}
-                                name="dogs"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Yes/No"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update if {data.cat.name} is use to children under the age of 8:</label><br />
-                            <input
-                                id='add-input'
-                                value={childU8}
-                                name="childU8"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Yes/No"
-                            />
-                        </div ><br />
-                        <div id='add-box'>
-                            <label>Update if {data.cat.name} is use to children between the ages of 8 to 13?</label><br />
-                            <input
-                                id='add-input'
-                                value={child8to13}
-                                name="child8t13"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Yes/No"
-                            />
-                        </div><br />
-                        <div id='add-box'>
-                            <label>Update any Special Requirements for {data.cat.name}:</label><br />
-                            <input
-                                id='add-input'
-                                value={specialReq}
-                                name="specialReq"
-                                onChange={handleInputChange}
-                                type="text"
-                                placeholder="Any other special needs?"
-                            />
-                        </div><br />
-                        <button id='add-button' onClick={handleFormSubmit}>Submit</button>
-                        <button id='add-button' onClick={removeCatProfile}>Delete</button>
-                    </form><br />
+                    <p id='edit-cat-title'>Delete {data.cat.name}</p>
+                    {removeError && <div>{removeError.message}</div>}
+                    {removeData && <div>The cat has been succesfully removed!</div>}
+                    {error && <div>{error.message}</div>}
+                    <p>Delete {data.cat.name} from the database?</p>
+                    <button id='edit-button' onClick={removeCatProfile}>Delete</button>
                 </div>
             </Fragment>)}
-            {error && <div>{error.message}</div>}
-            {updateError && <div>{updateError.message}</div>}
-            {removeError && <div>{removeError.message}</div>}
-            {removeData && <div>The data has been deleted!</div>}
             <br /></main>
     );
 };
