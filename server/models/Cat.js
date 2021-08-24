d1425a7da336e2af142cff843a14c3f8eba858a2
Kellie Ryan <littlekel03@gmail.com>
2021-08-24 13:33:36 +0800

    Updated cat schema

--- server/models/Cat.js
+++ server/models/Cat.js
@@ -3,39 +3,30 @@ const { Schema, model } = require('mongoose');
 const catSchema = new Schema ({
     name: {
         type: String,
-        required: true,
     },
     dob: {
         type: String,
-        required: true,
     },
     sex: {
         type: String,
-        required: true,
     },
     shy: {
         type: String,
-        required: true,
     },
     otherCats:{
         type: String,
-        required: true,
     },
     dogs: {
         type: String,
-        required: true,
     },
     childU8: {
         type: String,
-        required: true,
     },
     child8to13: {
         type: String,
-        required: true,
     },
     specialReq: {
         type: String,
-        required: true,
     },
     image: {
         type: String,
